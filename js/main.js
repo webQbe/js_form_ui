5fb7711908aa65ce2b6896c8b71afbe74349e78b
webQbe <webcubesolutionslk@gmail.com>
2024-10-22 20:07:26 +0530

    Added regex to match an email address

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const questions = [
 
     { question: 'Enter Your First Name' },
     { question: 'Enter Your Last Name' },
-    { question: 'Enter Your Email' }, 
+    { question: 'Enter Your Email', pattern: '/\S+@\S+\.\S+/' }, // Add regex to match email address
     { question: 'Create A Password' }
 
 ];
