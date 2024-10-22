8ffb4d3a079ea57ca7dbda824262291beb6c20f2
webQbe <webcubesolutionslk@gmail.com>
2024-10-22 20:06:34 +0530

    Created questions array

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Questions Array
+const questions = [
+
+    { question: 'Enter Your First Name' },
+    { question: 'Enter Your Last Name' },
+    { question: 'Enter Your Email' }, 
+    { question: 'Create A Password' }
+
+];
